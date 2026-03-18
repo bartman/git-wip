067a15627f032e42b13a68f528a4a3fd0fc81ddf
Jake Coble <13357401+jakecoble@users.noreply.github.com>
2026-03-18 18:17:09 -0400

    Add copyright notices

--- sublime/gitwip.py
+++ sublime/gitwip.py
@@ -1,3 +1,20 @@
+# Copyright Bart Trojanowski <bart@jukie.net>
+#
+# This file is part of git-wip.
+#
+# git-wip is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# git-wip is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with git-wip. If not, see <https://www.gnu.org/licenses/>.
+
 import sublime_plugin
 from subprocess import Popen, PIPE, STDOUT
 import os
