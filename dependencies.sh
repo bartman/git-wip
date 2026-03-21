6b2eba751e2640f5aff27759dd3f6be43a97a4c0
Bart Trojanowski <bart@jukie.net>
2026-03-21 20:45:33 +0000

    dependencies.sh prefers clang over gcc

--- dependencies.sh
+++ dependencies.sh
@@ -3,7 +3,22 @@
 SUDO=sudo
 [ "$(id -u)" = 0 ] && SUDO=
 
-function best() {
+function die() {
+    echo >&2 "ERROR: $*"
+    exit 1
+}
+
+function must_have_one_of() {
+    for n in "$@" ; do
+        if apt policy "$n" >/dev/null 2>&1 ; then
+            echo "$n"
+            return
+        fi
+    done
+    die "apt cannot find any of these packages: $*"
+}
+
+function want_one_of() {
     for n in "$@" ; do
         if apt policy "$n" >/dev/null 2>&1 ; then
             echo "$n"
@@ -12,20 +27,22 @@ function best() {
     done
 }
 
+packages=(
+    cmake
+    $(want_one_of clangd)
+    $(must_have_one_of "clang" "gcc")
+    $(must_have_one_of "clang" "g++")
+    git
+    pkg-config
+    googletest
+    libgmock-dev
+    libgtest-dev
+    libgit2-dev
+    make
+    ninja-build
+)
+
 set -e -x
 $SUDO apt update
-$SUDO apt install -y \
-    cmake \
-    clangd \
-    clang \
-    g++ \
-    gcc \
-    git \
-    pkg-config \
-    googletest \
-    libgmock-dev \
-    libgtest-dev \
-    libgit2-dev \
-    make \
-    ninja-build \
+$SUDO apt install -y "${packages[@]}"
 
